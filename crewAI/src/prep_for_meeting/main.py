c591388d9d5c6c26b58a0970c539ca53dd2b038e
Titus Lim <tituslhy@gmail.com>
2025-04-02 22:09:22 +0800

    initial commit

---
+++ crewAI/src/prep_for_meeting/main.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import sys
+
+from prep_for_meeting.crew import PrepForMeetingCrew
+
+# This main file is intended to be a way for your to run your
+# crew locally, so refrain from adding necessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+
+def run():
+    """
+    Run the crew.
+    """
+
+    inputs = {
+        "participants": [
+            "List of participants name and email, e.g. 'John Doe <john.doe@gmail.com>'"
+        ],
+        "company": "<Company name>",
+        "context": "<Context of the meeting>",
+        "objective": "<Objective of the meeting>",
+        "prior_interactions": "<Prior interactions with the participants>",
+    }
+
+    PrepForMeetingCrew().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {"topic": "AI LLMs"}
+    try:
+        PrepForMeetingCrew().crew().train(
+            n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs
+        )
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        PrepForMeetingCrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {"topic": "AI LLMs"}
+    try:
+        PrepForMeetingCrew().crew().test(
+            n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs
+        )
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
